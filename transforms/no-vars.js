1d22f0ca126d3c0602cf6871d1af38bd8ca6b5db
Daniel L <maxcore1120@gmail.com>
2025-10-18 20:12:22 +0200

    Convert `var` to `let` or `const

--- transforms/no-vars.js
+++ transforms/no-vars.js
@@ -1,232 +1,194 @@
-module.exports = function(file, api) {
-  const j = api.jscodeshift;
+/**
+ * Codemod: Convert `var` to `let` or `const`
+ * Logic:
+ * - Keeps `var` if it’s used before declaration, used in closures, or redeclared.
+ * - Converts to `let` if mutated (reassigned or updated).
+ * - Converts to `const` otherwise.
+ */
 
+module.exports = function (file, api) {
+  const j = api.jscodeshift;
   const root = j(file.source);
 
-  const TOP_LEVEL_TYPES = [
+  const TOP_LEVEL_TYPES = new Set([
     'Function',
     'FunctionDeclaration',
     'FunctionExpression',
     'ArrowFunctionExpression',
     'Program',
-  ];
-  const FOR_STATEMENTS = [
-    'ForStatement',
-    'ForOfStatement',
-    'ForInStatement',
-  ];
-  const getScopeNode = blockScopeNode => {
-    let scopeNode = blockScopeNode;
-    let isInFor = FOR_STATEMENTS.indexOf(blockScopeNode.value.type) !== -1;
-    while (TOP_LEVEL_TYPES.indexOf(scopeNode.node.type) === -1) {
-      scopeNode = scopeNode.parentPath;
-      isInFor = isInFor || FOR_STATEMENTS.indexOf(scopeNode.value.type) !== -1;
+  ]);
+  const FOR_STATEMENTS = new Set(['ForStatement', 'ForOfStatement', 'ForInStatement']);
+
+  const getScopeNode = (path) => {
+    let scope = path;
+    let isInFor = FOR_STATEMENTS.has(path.value.type);
+    while (!TOP_LEVEL_TYPES.has(scope.node.type)) {
+      scope = scope.parentPath;
+      if (FOR_STATEMENTS.has(scope.value.type)) isInFor = true;
     }
-    return {scopeNode, isInFor};
-  };
-  const findFunctionDeclaration = (node, container) => {
-    while (node.value.type !== 'FunctionDeclaration' && node !== container) {
-      node = node.parentPath;
-    }
-    return node !== container ? node : null;
-  };
-  const isForLoopDeclarationWithoutInit = declaration => {
-    const parentType = declaration.parentPath.value.type;
-    return parentType === 'ForOfStatement' || parentType === 'ForInStatement';
+    return { scope, isInFor };
   };
 
-  const extractNamesFromIdentifierLike = id => {
-    if (!id) {
-      return [];
-    } else if (id.type === 'ObjectPattern') {
-      return id.properties.map(
-        d => (d.type === 'SpreadProperty' ? [d.argument.name] : extractNamesFromIdentifierLike(d.value))
-      ).reduce((acc, val) => acc.concat(val), []);
-    } else if (id.type === 'ArrayPattern') {
-      return id.elements.map(extractNamesFromIdentifierLike).reduce((acc, val) => acc.concat(val), []);
-    } else if (id.type === 'Identifier') {
-      return [id.name];
-    } else if (id.type === 'RestElement') {
-      return [id.argument.name];
-    } else {
-      return [];
+  const extractNames = (id) => {
+    if (!id) return [];
+    switch (id.type) {
+      case 'Identifier':
+        return [id.name];
+      case 'RestElement':
+        return [id.argument.name];
+      case 'ObjectPattern':
+        return id.properties.flatMap((p) =>
+          p.type === 'SpreadProperty'
+            ? [p.argument.name]
+            : extractNames(p.value)
+        );
+      case 'ArrayPattern':
+        return id.elements.flatMap(extractNames);
+      default:
+        return [];
     }
   };
-  const getDeclaratorNames = (declarator) => {
-    return extractNamesFromIdentifierLike(declarator.id);
-  };
-  const isIdInDeclarator = (declarator, name) => {
-    return getDeclaratorNames(declarator).indexOf(name) !== -1;
-  };
-  const getLocalScope = (scope, parentScope) => {
-    const names = [];
-    while (scope !== parentScope) {
+
+  const getDeclaratorNames = (decl) => extractNames(decl.id);
+  const isIdInDeclarator = (decl, name) => getDeclaratorNames(decl).includes(name);
+
+  const getLocalScopeNames = (scope, parent) => {
+    const names = new Set();
+    while (scope !== parent) {
       if (Array.isArray(scope.value.body)) {
-        scope.value.body.forEach(node => {
+        for (const node of scope.value.body) {
           if (node.type === 'VariableDeclaration') {
-            node.declarations.map(getDeclaratorNames).forEach(dNames => {
-              dNames.forEach(name => {
-                if (names.indexOf(name) === -1) {
-                  names.push(name);
-                }
-              });
-            });
+            for (const decl of node.declarations) {
+              getDeclaratorNames(decl).forEach((n) => names.add(n));
+            }
           }
-        });
+        }
       }
       if (Array.isArray(scope.value.params)) {
-        scope.value.params.forEach(id => {
-          extractNamesFromIdentifierLike(id).forEach(name => {
-            if (names.indexOf(name) === -1) {
-              names.push(name);
-            }
-          });
-        });
+        for (const param of scope.value.params) {
+          extractNames(param).forEach((n) => names.add(n));
+        }
       }
       scope = scope.parentPath;
     }
-    return names;
-  };
-  const hasLocalDeclarationFor = (nodePath, parentScope, name) => {
-    return (
-      getLocalScope(nodePath, parentScope).indexOf(name) !== -1
-    );
+    return [...names];
   };
 
-  const isTruelyVar = (node, declarator) => {
-    const blockScopeNode = node.parentPath;
-    const {scopeNode, isInFor} = getScopeNode(blockScopeNode);
+  const hasLocalDeclaration = (node, parentScope, name) =>
+    getLocalScopeNames(node, parentScope).includes(name);
 
-    // if we are in a for loop of some kind, and the variable
-    // is referenced within a closure, revert to `var`
-    // It would be safe to do the conversion if you can verify
-    // that the callback is run synchronously
-    const isUsedInClosure = (
+  const findFunctionDeclaration = (node, container) => {
+    while (node.value.type !== 'FunctionDeclaration' && node !== container)
+      node = node.parentPath;
+    return node !== container ? node : null;
+  };
+
+  const isTrulyVar = (node, declarator) => {
+    const block = node.parentPath;
+    const { scope, isInFor } = getScopeNode(block);
+
+    // var inside for-loop closure → unsafe
+    const usedInClosure =
       isInFor &&
-      j(blockScopeNode).find(j.Function).filter(
-        functionNode => (
-          j(functionNode).find(j.Identifier).filter(
-            id => isIdInDeclarator(declarator, id.value.name)
-          ).size() !== 0
-        )
-      ).size() !== 0
-    );
-
-    // if two attempts are made to declare the same variable,
-    // revert to `var`
-    // TODO: if they are in different block scopes, it may be
-    //       safe to convert them anyway
-    const isDeclaredTwice = j(scopeNode)
-      .find(j.VariableDeclarator)
-      .filter(otherDeclarator => {
-        return (
-          otherDeclarator.value !== declarator &&
-          getScopeNode(otherDeclarator).scopeNode === scopeNode &&
-          getDeclaratorNames(otherDeclarator.value).some(
-            name => isIdInDeclarator(declarator, name)
-          )
-        );
-      }).size() !== 0;
-
-    return isUsedInClosure || isDeclaredTwice || j(scopeNode)
-      .find(j.Identifier)
-      .filter(n => {
-        if (!isIdInDeclarator(declarator, n.value.name)) {
-          return false;
-        }
-        // If the variable is used in a function declaration that gets
-        // hoisted, it could get called early
-        const functionDeclaration = findFunctionDeclaration(n, scopeNode);
-        const isCalledInHoistedFunction = (
-          functionDeclaration &&
-          j(scopeNode)
+      j(block)
+        .find(j.Function)
+        .some((fn) =>
+          j(fn)
             .find(j.Identifier)
-            .filter(n => {
-              return (
-                n.value.name === functionDeclaration.value.id.name &&
-                n.value.start < declarator.start
-              );
-            }).size() !== 0
+            .some((id) => isIdInDeclarator(declarator, id.value.name))
         );
-        if (isCalledInHoistedFunction) {
-          return true;
-        }
-        const referenceScope = getScopeNode(n.parent).scopeNode;
-        if (
-          referenceScope === scopeNode ||
-          !hasLocalDeclarationFor(n, scopeNode, n.value.name)
-        ) {
-          // if the variable is referenced outside the current block
-          // scope, revert to using `var`
-          const isOutsideCurrentScope = (
-            j(blockScopeNode).find(j.Identifier).filter(
-              innerNode => innerNode.node.start === n.node.start
-            ).size() === 0
-          );
 
-          // if a variable is used before it is declared, revert to
-          // `var`
-          // TODO: If `isDeclaredTwice` is improved, and there is
-          //       another declaration for this variable, it may be
-          //       safe to convert this anyway
-          const isUsedBeforeDeclaration = (
-            n.value.start < declarator.start
-          );
+    // redeclared variable → unsafe
+    const redeclared = j(scope)
+      .find(j.VariableDeclarator)
+      .filter(
+        (d) =>
+          d.value !== declarator &&
+          getScopeNode(d).scope === scope &&
+          getDeclaratorNames(d.value).some((n) =>
+            isIdInDeclarator(declarator, n)
+          )
+      )
+      .size() > 0;
+
+    if (usedInClosure || redeclared) return true;
+
+    // references before declaration or outside scope
+    return (
+      j(scope)
+        .find(j.Identifier)
+        .filter((id) => {
+          if (!isIdInDeclarator(declarator, id.value.name)) return false;
+
+          const funcDecl = findFunctionDeclaration(id, scope);
+          if (
+            funcDecl &&
+            j(scope)
+              .find(j.Identifier)
+              .some(
+                (ref) =>
+                  ref.value.name === funcDecl.value.id?.name &&
+                  ref.value.start < declarator.start
+              )
+          ) {
+            return true;
+          }
+
+          const refScope = getScopeNode(id.parent).scope;
+          const isOutside =
+            j(block)
+              .find(j.Identifier)
+              .filter((inner) => inner.node.start === id.node.start)
+              .size() === 0;
+          const usedBefore = id.value.start < declarator.start;
 
           return (
-            isOutsideCurrentScope ||
-            isUsedBeforeDeclaration
+            refScope === scope && !hasLocalDeclaration(id, scope, id.value.name)
+              ? isOutside || usedBefore
+              : false
           );
-        }
-      }).size() > 0;
+        })
+        .size() > 0
+    );
   };
 
-  /**
-   * isMutated utility function to determine whether a VariableDeclaration
-   * contains mutations. Takes an optional VariableDeclarator node argument to
-   * return only whether that specific Identifier is mutated
-   *
-   * @param {ASTPath} node VariableDeclaration path
-   * @param {ASTNode} [declarator] VariableDeclarator node
-   * @return {Boolean}
-   */
-  const isMutated = (node, declarator) => {
-    const scopeNode = node.parent;
-
-    const hasAssignmentMutation = j(scopeNode)
-      .find(j.AssignmentExpression)
-      .filter(n => {
-        return extractNamesFromIdentifierLike(n.value.left).some(name => {
-          return isIdInDeclarator(declarator, name);
-        });
-      }).size() > 0;
-
-    const hasUpdateMutation = j(scopeNode)
-      .find(j.UpdateExpression)
-      .filter(n => {
-        return isIdInDeclarator(declarator, n.value.argument.name);
-      }).size() > 0;
-
-    return hasAssignmentMutation || hasUpdateMutation;
+  const isMutated = (node, decl) => {
+    const scope = node.parent;
+    return (
+      j(scope)
+        .find(j.AssignmentExpression)
+        .some((n) =>
+          extractNames(n.value.left).some((name) =>
+            isIdInDeclarator(decl, name)
+          )
+        ) ||
+      j(scope)
+        .find(j.UpdateExpression)
+        .some((n) => isIdInDeclarator(decl, n.value.argument.name))
+    );
   };
 
-  const updatedAnything = root.find(j.VariableDeclaration).filter(
-    dec => dec.value.kind === 'var'
-  ).filter(declaration => {
-    return declaration.value.declarations.every(declarator => {
-      return !isTruelyVar(declaration, declarator);
-    });
-  }).forEach(declaration => {
-    const forLoopWithoutInit = isForLoopDeclarationWithoutInit(declaration);
-    if (
-      declaration.value.declarations.some(declarator => {
-        return (!declarator.init && !forLoopWithoutInit) || isMutated(declaration, declarator);
-      })
-    ) {
-      declaration.value.kind = 'let';
-    } else {
-      declaration.value.kind = 'const';
-    }
-  }).size() !== 0;
-  return updatedAnything ? root.toSource() : null;
-}
+  const updated = root
+    .find(j.VariableDeclaration)
+    .filter((d) => d.value.kind === 'var')
+    .forEach((decl) => {
+      const allSafe = decl.value.declarations.every(
+        (dec) => !isTrulyVar(decl, dec)
+      );
+      if (!allSafe) return;
+
+      const needsLet = decl.value.declarations.some(
+        (d) =>
+          (!d.init &&
+            !['ForOfStatement', 'ForInStatement'].includes(
+              decl.parentPath.value.type
+            )) ||
+          isMutated(decl, d)
+      );
+
+      decl.value.kind = needsLet ? 'let' : 'const';
+    })
+    .size() > 0;
+
+  return updated ? root.toSource() : null;
+};
